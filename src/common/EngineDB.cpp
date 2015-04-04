cee99bd850cc203fdec5f9c5e9d4644ecc415549
Wenzheng Li <wenzhenl@usc.edu>
2015-04-04 11:52:19 -0700

    Begin implementing the class EngineDB to handle the MySql engine database

---
+++ src/common/EngineDB.cpp
@@ -0,0 +1,27 @@
+/*
+ *    Part of AMORDAD software
+ *
+ *    Copyright (C) 2014 University of Southern California,
+ *                       Andrew D. Smith and Wenzheng Li
+ *
+ *    Authors: Andrew D. Smith, Wenzheng Li
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "EngineDB.hpp"
+
+EngineDB::EngineDB(const std::string db, const std::string server, 
+                   const std::string user, const std::string pass) {
+  conn
+}
