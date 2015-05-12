52ebd4aef9fd35e561a7864b1f23825d2de62076
Wenzheng Li <wenzhenl@usc.edu>
2015-05-12 15:51:09 -0700

    Keep Amordad standalone
    Amordad is a standalone content based big data indexing system.
    All the code in this repo should be only system related,
    code related to specific application should go to specific repo

--- website/refresh.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $url = "http://localhost:18080/refresh";
-  $file = file_get_contents($url);
-  $result = json_decode($file, true);
-  $response = "Successfully refreshed!\n"
-            . "It takes ".$result['time']." seconds\n";
-  echo $response;
-?>
